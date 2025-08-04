faa7c61bf213ae205027a77d4c9c993d8ac3105f
shahadathhs <shahadathhossensajib732@gmail.com>
2025-08-04 06:30:29 +0600

    feat: variable expolered

--- basic/variables.go
+++ basic/variables.go
@@ -2,9 +2,44 @@ package basic
 
 import "fmt"
 
+var globalVar int= 12
+
 func Variables() {
+	var init int
 	var a string = "Hello"
 	b := "World"
 	c := "!"
-	fmt.Println(a + " " + b + c)
+	init = 10
+	fmt.Println(a+" "+b+c, init)
+
+	// Default Values of each type
+	// Numeric = 0
+	var i int
+	var f float64
+	// Boolean = false
+	var b1 bool
+	// String = ""
+	var s string
+	// Pointer, Function, Interface, Slice, Map, Struct = nil
+	fmt.Printf("%v %v %v %q\n", i, f, b1, s)
+
+	// Scope
+	{
+		var x int = 10
+		fmt.Println(x)
+	}
+	x := 11
+	fmt.Println(x)
+	
+	fmt.Println("globalVar: ", globalVar)
+	
+	globalVar = 13
+	fmt.Println("globalVar: ", globalVar)
+	scopeExample()
+}
+
+func scopeExample() {
+	var x int = 10
+	fmt.Println(x)
+	fmt.Println("globalVar: ", globalVar)
 }
